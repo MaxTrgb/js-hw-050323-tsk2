9b313a8710a993e297c30727b970b3a24c85a28a
MaxTrgb <126002400+MaxTrgb@users.noreply.github.com>
2024-03-11 15:05:09 +0200

    got resizable blocks

--- script/index.js
+++ script/index.js
@@ -1,33 +1,75 @@
-$(document).ready(function() {
-    $(".toggle-arrow").click(function() {
-      var block = $(this).closest('.block');
-      if ($(this).hasClass('left')) {
-        block.toggleClass('hidden');
-        $(this).toggleClass('left right').html($(this).hasClass('left') ? '&lt;' : '&gt;');
-      } else {
-        var leftBlock = block.prev('.block');
-        leftBlock.toggleClass('hidden');
-        $(this).toggleClass('left right').html($(this).hasClass('left') ? '&lt;' : '&gt;');
-      }
-    });
-  
-    $(".resize-bar").mousedown(function(e) {
-      var block = $(this).closest('.block');
-      var startY = e.clientY;
-      var startHeight = parseInt(block.css('height'));
-  
-      $(document).mousemove(function(e) {
-        var deltaY = startY - e.clientY;
-        var newHeight = startHeight + deltaY;
-  
-        if (newHeight >= 100) {
-          block.css('height', newHeight + 'px');
-        }
-      });
-  
-      $(document).mouseup(function() {
-        $(document).off('mousemove');
-        $(document).off('mouseup');
-      });
+var isDragging = false,
+  resizingElement,
+  offsetX,
+  offsetY,
+  initialWidth,
+  initialHeight,
+  newWidth,
+  newHeight;
+
+const currentWidthValues = document.querySelectorAll(".current-width");
+const resizers = document.querySelectorAll(".resizer");
+const blocks = document.querySelectorAll(".block");
+const container = document.querySelector(".main-container");
+
+window.addEventListener("resize", updateCurrentWidthValues);
+
+document.addEventListener("DOMContentLoaded", () => {
+  updateCurrentWidthValues();
+  resizers.forEach((resizer) => {
+    resizer.addEventListener("mousedown", (e) => {
+      isDragging = true;
+      resizingElement = resizer.closest(".block");
+      resizingElement.classList.add("getting-resized");
+      initialWidth = resizingElement.offsetWidth;
+      initialHeight = resizingElement.offsetHeight;
+      offsetX = e.clientX;
+      offsetY = e.clientY;
+      container.style.cursor = "grabbing";
     });
   });
+});
+
+document.addEventListener("mousemove", (e) => {
+  if (isDragging) {
+    var newX = e.clientX - offsetX;
+    var newY = e.clientY - offsetY;
+    if (resizingElement.classList.contains("resize-bottom")) {
+      newHeight = initialHeight + newY + "px";
+      resizingElement.style.height = newHeight;
+    } else if (resizingElement.classList.contains("resize-top")) {
+      newHeight = initialHeight - newY + "px";
+      resizingElement.style.height = newHeight;
+    } else if (resizingElement.classList.contains("resize-left")) {
+      newWidth = initialWidth - newX + "px";
+      resizingElement.style.width = newWidth;
+    } else if (resizingElement.classList.contains("resize-right")) {
+      newWidth = initialWidth + newX + "px";
+      resizingElement.style.width = newWidth;
+    } else {
+      console.log("no direction specified in class name");
+    }
+  }
+  updateCurrentWidthValues();
+});
+
+document.addEventListener("mouseup", function (e) {
+  isDragging = false;
+  container.style.cursor = "auto";
+  blocks.forEach((block) => {
+    block.classList.remove("getting-resized");
+  });
+});
+
+function updateCurrentWidthValues() {
+  currentWidthValues.forEach((element) => {
+    let newText =
+      "Width: " +
+      element.closest(".block").offsetWidth +
+      "px," +
+      " Height: " +
+      element.closest(".block").offsetHeight +
+      "px";
+    element.textContent = newText;
+  });
+}
